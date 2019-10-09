7f0033f2bc521b3e80d52edcd9488c356087cd0b
Ricardo <rokoneitor@gmail.com>
2019-10-08 21:33:15 -0500

    corrección de evaluaciones de restricciones

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pplag
 Programa para resolver un problema de programación lineal mediante algoritmos genéticos
+
+El programa puede recibir un máximo de 5 variables y 5 restricciones
+El programa acepta restricciones <=, >= e =
+Las restricciones de tipo = tienen una tolerancia de error de 5%
--- sketch.js
+++ sketch.js
@@ -1,50 +1,65 @@
 function init(){
 
-	var var_ingresadas = document.getElementById("variables").value;
-	var variables = 2;	
-
 	// Construimos la función objetivo 12a + 5b
 	var fo = new funcionObjetivo([12, 5]);
-
-	// Construimos la restricción 2a + 3b <= 50
-
-	var rest = new Array(2);	
+	// Construimos las restricciones
+	var rest = new Array(2);
+	// a + b = 10
 	rest[0] = new restriccion([1,1], "=", 10);
+	// b >= 4
 	rest[1] = new restriccion([0,1], ">=", 4);
 
-	// Construimos una población de n especimenes
-
-	var cantidad = 2;
+	// Obtenemos la cantidad de variables que se tendrán
+	var variables = parseInt(document.getElementById("variables").value);
+	// Obtenemos la cantidad de elementos que tendrá la población
+	var cantidad = parseInt(document.getElementById("poblacion").value);
+	// Creamos el arreglo de la población
 	var poblacion = new Array(cantidad);
 
+	// Iniciamos los elementos de la población
 	for(let i = 0; i < cantidad; i++)
 		poblacion[i] = new especimen(variables);
 
-	// Evaluamos la restricción con la población generada
-	for(let j = 0; j < 2; j++)
-	{
-		for(let i = 0; i < cantidad; i++)
-		{
-			if(rest[j].evaluate(poblacion[i]) == true)
-				poblacion[i].setObtained(fo.evaluate(poblacion[i]));
-		}
-	}
-
+	// Mostramos a los especímenes
 	for(let i = 0; i < cantidad; i++)
 	{
 		poblacion[i].show();
-		console.log("-----------------------------");
+		console.log("------------------------");
 	}
 
+	// Evaluamos las restricciones
+	var m;
+
+	// Recorremos las especies
+	for(let i = 0; i < rest.length; i++)
+	{
+		for(let j = 0; j < cantidad; j++)
+		{
+			// Obtenemos el valor de evaluar la restricción
+			m = rest[i].evaluate(poblacion[j]);			
+
+			// Si se cumple entonces le asigna el valor de la función objetivo
+			if(m != false)
+				poblacion[j].setObtained(fo.evaluate(poblacion[j]));
+
+			// Si es falso le asigna false al resultado de la función objetivo
+			else if(m == false)
+			{
+				poblacion[j].setObtained(false);
+				i++;
+			}
+		}
+	}
+
+	// Mostramos a los especímenes que cumplieron las restricciones
 	for(let i = 0; i < cantidad; i++)
 	{
-		// Usar una variable aparte para evitar problemas de referencia
-		var ok = poblacion[i].getObtained();
-
-		if(ok != false)
+		// Variable para guardar dato obtenido
+		var check = poblacion[i].getObtained();
+		if(check != false)
 		{
-			console.log("Cumplió las restricciones con un valor de : " + ok);
+			console.log("Cumplió las condiciones con un valor de : " + check);
 			poblacion[i].show();
 		}
-	}	
+	}
 }
