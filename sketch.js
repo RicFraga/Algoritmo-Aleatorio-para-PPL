d9261a97039dbf0439b72594b5b93a5c5415ad54
Ricardo <rokoneitor@gmail.com>
2019-10-15 10:39:22 -0500

    corrección de método aleatorio

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 function init(){
 
 	// Construimos la función objetivo 12a + 5b
-	var fo = new funcionObjetivo([2, 2]);
+	var fo = new funcionObjetivo([4, 2]);
 	// Construimos las restricciones
-	var rest = new Array(4);
-	// a + 4y <= 25
-	rest[0] = new restriccion([1,4], "<=", 25);
-	// 2a + 3b <= 20
-	rest[1] = new restriccion([2,3], "<=", 20);
-	// a >= 10
-	rest[2] = new restriccion([1,0], ">=", 5);
-	// b >= 0
-	rest[3] = new restriccion([0,1], ">=", 0);
+	var rest = new Array(5);
+	// a + 2y >= 10
+	rest[0] = new restriccion([1,2], ">=", 10);
+	// 2a + b <= 25
+	rest[1] = new restriccion([2,1], "<=", 25);
+	// a >= 3
+	rest[2] = new restriccion([1,0], ">=", 3);
+	// b <= 10
+	rest[3] = new restriccion([0,1], "<=", 10);
+	//y >= 0
+	rest[4] = new restriccion([0,1], ">=", 0);
 
 	// Obtenemos la cantidad de variables que se tendrán
 	var variables = parseInt(document.getElementById("variables").value);
@@ -34,7 +36,7 @@ function init(){
 	// Evaluamos las restricciones
 	var m;
 	var max = -1;
-	var min = 9999999999;	
+	var min = 999999999999999;
 
 	// Empezamos recorriendo las especies
 	for(let i = 0; i < cantidad; i++)
@@ -62,21 +64,24 @@ function init(){
 	var indice_min;	
 
 	// Mostramos a los especímenes que cumplieron las restricciones
-	for(let i = 0; i < cantidad; i++)
+	for(let i = 0; i < poblacion.length; i++)
 	{
 		// Variable para guardar dato obtenido
-		var check = poblacion[i].getObtained();
+		var check = poblacion[i].getObtained();		
 
-		if(check > max)
+		if(check != false)
 		{
-			max = check;
-			indice_max = i;
-		}
+			if(check > max)
+			{
+				max = check;
+				indice_max = i;
+			}
 
-		if(check < min)
-		{
-			min = check;
-			indice_min = i;
+			if(check < min)
+			{
+				min = check;
+				indice_min = i;
+			}
 		}	
 	}
 
