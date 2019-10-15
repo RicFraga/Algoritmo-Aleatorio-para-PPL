1224115eee3d61f9bf4d8d4b68b13ad2628d497f
Ricardo <rokoneitor@gmail.com>
2019-10-15 11:31:41 -0500

    generados de números aleatorios

---
+++ cuadradosMedios.js
@@ -0,0 +1,19 @@
+class cuadradosMedios {
+	constructor(seed)
+	{
+		var squared = seed ** 2;
+		var squared_string = squared.toString();
+		var l = squared_string.length;
+		var random_string = ".";
+
+		for(let i = 2; i < l - 2; i++)
+			random_string += squared_string[i];		
+
+		this.random = parseFloat(random_string);
+	}
+
+	get()
+	{
+		return this.random;
+	}
+}
